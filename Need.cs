6e310bf6382006bdf86ed33ce5a0966fd2ea52ae
Quinten Marsala <quinten.marsala@gmail.com>
2022-07-25 16:30:40 -0700

    fix typo

--- Need.cs
+++ Need.cs
@@ -1,7 +1,7 @@
 public record Need
 {
     public string Id {get;init;} = Guid.NewGuid().ToString();
-    public string Name { get; init; } = "An uknown need";
+    public string Name { get; init; } = "An unknown need";
     public string Notes { get; init; } = string.Empty;
     public int Times { get; init; } = 1;
     public IEnumerable<DayOfWeek> Days { get; init; } = Enumerable.Empty<DayOfWeek>();
