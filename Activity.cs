5cd058e459eef0f51a505a27d322b12ae73fcc92
Quinten Marsala <quinten.marsala@gmail.com>
2022-07-25 15:52:44 -0700

    explicitly declare props

--- Activity.cs
+++ Activity.cs
@@ -1,12 +1,9 @@
 
-public record Activity(Need What, DateTime When, string Notes)
+public record Activity
 {
-    public static Activity New(Need what, DateTime? when, string? notes)
-        => (what, when, notes) switch
-        {
-            (Need w, DateTime t, string n) => new Activity(w, t, n),
-            (Need w, DateTime t, null) => new Activity(w, t, string.Empty),
-            (Need w, null, string n) => new Activity(w, DateTime.Now, n),
-            (Need w, null, null) => new Activity(w, DateTime.Now, string.Empty)
-        };
+    public string Id{get;init;} = Guid.NewGuid().ToString();
+    public string PetId {get;init;} = string.Empty;
+    public string NeedId { get; init; } = string.Empty;
+    public DateTime When { get; init; } = DateTime.Now;
+    public string Notes { get; init; } = string.Empty;
 }
