733fc4c0407947396b7148c520456d1e075d7af1
Quinten Marsala <quinten.marsala@gmail.com>
2022-07-25 13:21:42 -0700

    remove unused controller registration

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ using static PetsFunctions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
