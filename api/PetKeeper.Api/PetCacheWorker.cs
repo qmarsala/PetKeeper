31f61dbfdddb5017f3cfce02ec0bd2fe49dbd0a5
Quinten Marsala <quinten.marsala@gmail.com>
2022-08-08 18:27:12 -0700

    some functional kafka?
    - with no error handling //todo

--- api/PetKeeper.Api/PetCacheWorker.cs
+++ api/PetKeeper.Api/PetCacheWorker.cs
@@ -1,10 +1,93 @@
 ﻿using Confluent.Kafka;
+using static LanguageExt.Prelude;
 using PetKeeper.Core;
 using PetKeeper.Infrastructure;
 using StackExchange.Redis;
 using System.Text.Json;
+using LanguageExt;
+using static PetKeeper.Api.TestGround;
+using Err = LanguageExt.Common.Error;
 
 namespace PetKeeper.Api;
+
+//some of this went backwards and thats ok
+// playing around with some function stuff (and failing a little)
+// todo: keep going down this road
+
+// need to explore "environments" with the effects
+// need to explore aff's (async effects)
+// need to improve redis interaction
+
+public static class TestGround
+{
+    // enumerator ???
+    public static IEnumerable<ConsumeResult<TKey, TValue>> ReadPetTopic<TKey, TValue>(this IConsumer<TKey, TValue> consumer, CancellationToken stoppingToken)
+    {
+        // err handling with either?
+        yield return consumer.Consume(stoppingToken);
+    }
+
+    // eff monads? - probably should be affs to be async
+    //public static Eff<ConsumeResult<string, string>> ReadTopic(IConsumer<string, string> consumer, CancellationToken stoppingToken)
+    //    => Eff(() => consumer.Consume(stoppingToken));
+    public static Eff<ConsumeResult<string, string>> LogValue(ConsumeResult<string, string> value)
+       => Eff(() => { Console.WriteLine($"{value.Message.Key}:{value.Message.Value}"); return value; });
+
+    // how could we improve the redis part of this?
+    // a redis aff? - can we use effs and affs together?
+    public static Eff<ConsumeResult<string, string>> UpdatePet(ConsumeResult<string, string> value, IDatabase db)
+        => Eff(() =>
+        {
+            var key = value.Message.Key;
+            var petJson = value.Message.Value;
+            var offset = value.Offset;
+            var cachedPetJson = db.StringGetAsync(key as string).Result;
+            var cachedPet = cachedPetJson.HasValue
+                ? JsonSerializer.Deserialize<CachedPet>(cachedPetJson!)
+                : new CachedPet();
+
+            // this isn't really good enough, 
+            // the offset could be lesser for a newer event 
+            // if we have increased partitions or versioned our topic
+            // todo: need something better
+            if (cachedPet?.Offset < offset)
+            {
+                var updatedPet = JsonSerializer.Deserialize<Pet>(petJson);
+                var updatedPetJson = JsonSerializer.Serialize(new CachedPet { Pet = updatedPet!, Offset = offset });
+                var petToRemove = db.StringGetAsync(key).Result;
+                // delete dup has made it back
+                if (petToRemove.HasValue)
+                {
+                    var positions = db.ListPositionsAsync("pets", petToRemove, 1).Result;
+                    if (positions.Any())
+                    {
+                        var r = db.ListRemoveAsync("pets", petToRemove).Result;
+                    }
+                    var d = db.KeyDeleteAsync(key).Result;
+                }
+                var s = db.StringSetAsync(key, updatedPetJson).Result;
+                var lp = db.ListLeftPushAsync("pets", updatedPetJson).Result;
+            }
+            return value;
+        });
+
+    public static Eff<ConsumeResult<string, string>> RemovePet(ConsumeResult<string, string> value, IDatabase db)
+        => Eff(() =>
+        {
+            var key = value.Message.Key;
+            var petToRemove = db.StringGetAsync(key).Result;
+            if (petToRemove.HasValue)
+            {
+                var positions = db.ListPositionsAsync("pets", petToRemove, 1).Result;
+                if (positions.Any())
+                {
+                    var r = db.ListRemoveAsync("pets", petToRemove).Result;
+                }
+                var d = db.KeyDeleteAsync(key).Result;
+            }
+            return value;
+        });
+}
 
 public class PetCacheWorker : BackgroundService
 {
@@ -17,7 +100,7 @@ public class PetCacheWorker : BackgroundService
     public IConsumer<string, string> Consumer { get; }
     public IConnectionMultiplexer Redis { get; }
 
-    //todo: challenge: functional?
+    //challenge: functional | inprogress
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         //todo:
@@ -27,60 +110,35 @@ public class PetCacheWorker : BackgroundService
 
         Consumer.Subscribe(KafkaTopics.Pets);
         var db = Redis.GetDatabase();
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            try
-            {
-                var result = Consumer.Consume(stoppingToken);
-                if (result.IsPartitionEOF) continue;
+        // I think we are missing something here...
+        // not sure how the runtime is really supposed to work and I think we are missing the value with this
+        // implementation
+        // also not sure why we need to pass it to run unit - makes me think the closure is bad 
+        // and there should be some rt variable for us... ???
+        var liveKafka = new LiveRuntime(Consumer);
+        var operation =
+            repeat(
+                from value in liveKafka.KafkaEff.Map(c => c.Consume(stoppingToken))
+                from _1 in LogValue(value)
+                from _2 in value.Message.Value is null
+                    ? RemovePet(value, db)
+                    : UpdatePet(value, db)
+                from _3 in liveKafka.KafkaEff.Map(c => c.StoreOffset(value))
+                select _3);
+        operation.RunUnit(liveKafka);
 
-                var key = result.Message.Key;
-                var petJson = result.Message.Value;
-                await (petJson is null 
-                    ? RemovePet(db, key) 
-                    : UpdatePet(db, key, petJson, result.Offset));
-
-                Consumer.StoreOffset(result);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-    }
-
-    private async Task RemovePet(IDatabase db, string key)
-    {
-        var petToRemove = await db.StringGetAsync(key);
-        if (petToRemove.HasValue)
-        {
-            var positions = await db.ListPositionsAsync("pets", petToRemove, 1);
-            if (positions.Any())
-            {
-                await db.ListRemoveAsync("pets", petToRemove);
-            }
-            await db.KeyDeleteAsync(key);
-        }
-    }
-
-    private async Task UpdatePet(IDatabase db, string key, string petJson, long offset)
-    {
-        var cachedPetJson = await db.StringGetAsync(key);
-        var cachedPet = cachedPetJson.HasValue
-            ? JsonSerializer.Deserialize<CachedPet>(cachedPetJson!)
-            : new CachedPet();
-
-        // this isn't really good enough, 
-        // the offset could be lesser for a newer event 
-        // if we have increased partitions or versioned our topic
-        // todo: need something better
-        if (cachedPet?.Offset < offset)
-        {
-            var updatedPet = JsonSerializer.Deserialize<Pet>(petJson);
-            var updatedPetJson = JsonSerializer.Serialize(new CachedPet { Pet = updatedPet!, Offset = offset });
-            await RemovePet(db, key);
-            await db.StringSetAsync(key, updatedPetJson);
-            await db.ListLeftPushAsync("pets", updatedPetJson);
-        }
+        // using the "non environment" eff 
+        var useOperation1 = false;
+        var operation1 =
+            repeat(
+                from _g in guard(useOperation1, Err.New("operation disabled"))
+                from value in KafkaEff.consumeTopic(Consumer)
+                from _1 in LogValue(value)
+                from _2 in value.Message.Value is null
+                    ? RemovePet(value, db)
+                    : UpdatePet(value, db)
+                from _3 in KafkaEff.storeOffset(Consumer, value)
+                select _3);
+        operation1.RunUnit();
     }
 }
---
+++ api/PetKeeper.Infrastructure/KafkaEff.cs
@@ -0,0 +1,71 @@
+﻿using Confluent.Kafka;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace PetKeeper.Infrastructure;
+
+// an attempt at functional kafka
+
+public interface KafkaIO
+{
+    ConsumeResult<string, string> Consume(CancellationToken cancellationToken);
+    Unit StoreOffset(ConsumeResult<string, string> result);
+}
+
+public interface HasKafka<RT>
+    where RT : struct, HasKafka<RT>
+{
+    Eff<RT, KafkaIO> KafkaEff { get; }
+}
+
+public struct LiveRuntime : HasKafka<LiveRuntime>
+{
+    private IConsumer<string, string> Consumer;
+
+    public LiveRuntime(IConsumer<string, string> consumer)
+    {
+        Consumer = consumer;
+    }
+
+    public Eff<LiveRuntime, KafkaIO> KafkaEff => SuccessEff(LiveKafkaIO.Create(Consumer));
+}
+
+//maybe?
+// how are we supposed to capture this IO?
+public struct LiveKafkaIO : KafkaIO
+{
+    private IConsumer<string, string> Consumer;
+
+    public LiveKafkaIO(IConsumer<string, string> consumer)
+    {
+        Consumer = consumer;
+    }
+
+    public static KafkaIO Create(IConsumer<string, string> consumer)
+    {
+        return new LiveKafkaIO(consumer);
+    }
+
+    public ConsumeResult<string, string> Consume(CancellationToken cancellationToken) => 
+        Consumer.Consume(cancellationToken);
+
+    public Unit StoreOffset(ConsumeResult<string, string> result)
+    {
+        Consumer.StoreOffset(result);
+        return unit;
+    }
+}
+
+// this seemed simpler - but the runtime is supposed to be cool?
+public static class KafkaEff
+{
+    public static Eff<ConsumeResult<string, string>> consumeTopic(IConsumer<string, string> consumer) =>
+        Eff(() => consumer.Consume());
+
+    public static Eff<Unit> storeOffset(IConsumer<string, string> consumer, ConsumeResult<string, string> result) =>
+        Eff(() =>
+        {
+            consumer.StoreOffset(result);
+            return unit;
+        });
+}
