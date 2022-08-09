d836b11aa7789a31e3ba0534b660b510da8344fc
Quinten Marsala <quinten.marsala@gmail.com>
2022-08-08 21:07:17 -0700

    some cleanup

--- api/PetKeeper.Api/PetCacheWorker.cs
+++ api/PetKeeper.Api/PetCacheWorker.cs
@@ -14,22 +14,9 @@ namespace PetKeeper.Api;
 // playing around with some function stuff (and failing a little)
 // todo: keep going down this road
 
-// need to explore "environments" with the effects
-// need to explore aff's (async effects)
-// need to improve redis interaction
-
 public static class TestGround
 {
-    // enumerator ???
-    public static IEnumerable<ConsumeResult<TKey, TValue>> ReadPetTopic<TKey, TValue>(this IConsumer<TKey, TValue> consumer, CancellationToken stoppingToken)
-    {
-        // err handling with either?
-        yield return consumer.Consume(stoppingToken);
-    }
 
-    // eff monads? - probably should be affs to be async
-    //public static Eff<ConsumeResult<string, string>> ReadTopic(IConsumer<string, string> consumer, CancellationToken stoppingToken)
-    //    => Eff(() => consumer.Consume(stoppingToken));
     public static Eff<ConsumeResult<string, string>> LogValue(ConsumeResult<string, string> value)
        => Eff(() => { Console.WriteLine($"{value.Message.Key}:{value.Message.Value}"); return value; });
 }
@@ -53,26 +40,26 @@ public class PetCacheWorker : BackgroundService
         // need to figure out a better way for this
         await Task.Delay(100, stoppingToken);
 
-        Consumer.Subscribe(KafkaTopics.Pets);
-        var db = Redis.GetDatabase();
         // I think we are missing something here...
         // not sure how the runtime is really supposed to work and I think we are missing the value with this
         // implementation
         // also not sure why we need to pass it to run unit - makes me think the closure is bad 
         // and there should be some rt variable for us... ???
-        var liveRT = new LiveRuntime(Consumer, Redis);
+        Consumer.Subscribe(KafkaTopics.Pets);
+        var runtime = new LiveRuntime(Consumer, Redis);
         var operation =
             repeat(
-                from value in liveRT.KafkaEff.Map(c => c.Consume(stoppingToken))
+                from value in runtime.KafkaEff.Map(c => c.Consume(stoppingToken))
                 from _1 in LogValue(value)
                 from _2 in value.Message.Value is null
-                    ? liveRT.RedisEff.Map(r => r.RemovePet(value))
-                    : liveRT.RedisEff.Map(r => r.UpdatePet(value))
-                from _3 in liveRT.KafkaEff.Map(c => c.StoreOffset(value))
+                    ? runtime.RedisEff.Map(r => r.RemovePet(value))
+                    : runtime.RedisEff.Map(r => r.UpdatePet(value))
+                from _3 in runtime.KafkaEff.Map(c => c.StoreOffset(value))
                 select _3);
-        operation.RunUnit(liveRT);
+        operation.RunUnit(runtime);
 
         // using the "non environment" eff 
+        var db = Redis.GetDatabase();
         var useOperation1 = false;
         var operation1 =
             repeat(
